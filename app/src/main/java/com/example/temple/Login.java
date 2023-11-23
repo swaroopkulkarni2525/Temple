267da4f9136aba6036afebd78ca5c633ca66b6e2
swaroop kulkarni <swaroopkulkarni761@gmail.com>
2023-11-23 16:56:09 +0530

    initial commit

--- app/src/main/java/com/example/temple/Login.java
+++ app/src/main/java/com/example/temple/Login.java
@@ -2,9 +2,12 @@ package com.example.temple;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
     Button Login,Signup;
@@ -17,6 +20,14 @@ public class Login extends AppCompatActivity {
         Login=findViewById(R.id.llgoin);
         Signup=findViewById(R.id.lsignup);
         username=findViewById(R.id.luname);
+        Signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Login.this, "signup now ", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(Login.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
