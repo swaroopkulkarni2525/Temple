2209dc946efb2623c2c985665a68e5121dfbea34
Johnyy16 <smnitin6@gmail.com>
2023-11-23 13:42:23 +0530

    updated

--- app/src/main/java/com/example/temple/Login.java
+++ app/src/main/java/com/example/temple/Login.java
@@ -3,12 +3,20 @@ package com.example.temple;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    Button Login,Signup;
+    TextView username,passowrd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Login=findViewById(R.id.llgoin);
+        Signup=findViewById(R.id.lsignup);
+        username=findViewById(R.id.luname);
+
     }
 }
